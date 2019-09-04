c20d291f7576727d5312e56e57e6bc538507e837
kittayanun <s5901012630016@email.kmutnb.ac.th>
2019-09-05 05:21:56 +0700

    show google map and marker

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
 
 declare var google;
 
@@ -10,13 +11,17 @@ declare var google;
 })
 export class HomePage implements OnInit {
 
-  constructor( private geolocation: Geolocation) {}
+  constructor( private geolocation: Geolocation, private loadingCtrl: LoadingController) {}
 
   ngOnInit(){
     this.loadMap();
   }
 
   async loadMap(){
+
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+
     const rta = await this.geolocation.getCurrentPosition();
     const myLatLng = {
       lat: rta.coords.latitude,
@@ -26,7 +31,21 @@ export class HomePage implements OnInit {
     const mapEle: HTMLElement = document.getElementById('map');
     const map = new google.maps.Map(mapEle, {
       center: myLatLng,
-      zoom: 12
+      zoom: 15
     });
+
+    google.maps.event
+    .addListenerOnce(map, 'idle', () => {
+      loading.dismiss();
+      const marker = new google.maps.Marker({
+        position: {
+          lat: myLatLng.lat,
+          lng: myLatLng.lng
+        },
+        map: map,
+        title: 'hello world'
+      });
+    } );
   }
+
 }
