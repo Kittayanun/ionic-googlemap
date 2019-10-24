3f34e7f384aa32150a4251edbd18e3a6a4c66d98
kittayanun <s5901012630016@email.kmutnb.ac.th>
2019-10-25 02:40:06 +0700

    fcm send notification and get token

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { FCM } from '@ionic-native/fcm/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     StatusBar,
     SplashScreen,
     Geolocation,
+    FCM,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { LoadingController } from '@ionic/angular';
-
+import { FCM } from '@ionic-native/fcm/ngx';
 declare var google;
 
 @Component({
@@ -11,9 +11,40 @@ declare var google;
 })
 export class HomePage implements OnInit {
 
-  constructor( private geolocation: Geolocation, private loadingCtrl: LoadingController) {}
+  constructor( private geolocation: Geolocation, private loadingCtrl: LoadingController, private fcm: FCM) {}
 
-  ngOnInit(){
+  getToken() {
+    this.fcm.getToken().then(token => {
+      console.log('get token:' + token);
+      // Register your new token in your back-end if you want
+      // backend.registerToken(token);
+    });
+  }
+
+  ngOnInit() {
+    this.fcm.subscribeToTopic('marketing');
+
+    this.fcm.getToken().then(token => {
+      console.log('fcm token:' + token);
+      //backend.registerToken(token);
+    });
+
+    this.fcm.onNotification().subscribe(data => {
+      console.log('data:' + data);
+      if (data.wasTapped) {
+        console.log('Received in background');
+      } else {
+        console.log('Received in foreground');
+      }
+    });
+
+    this.fcm.onTokenRefresh().subscribe(token => {
+      console.log('Refreshtoken:' + token);
+      //backend.registerToken(token);
+    });
+
+    this.fcm.unsubscribeFromTopic('marketing');
+
     this.loadMap();
   }
 
